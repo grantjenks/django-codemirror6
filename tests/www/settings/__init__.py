105fd27776fdcdf9e7eb2aaa7cfd7ac05917fa31
Grant Jenks <grant.jenks@gmail.com>
2022-09-21 12:13:36 -0700

    i blue it

--- tests/www/settings/__init__.py
+++ tests/www/settings/__init__.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x6q798#ez+ep(p)%d+1#z+88ptc-xz+=!f098=12j(a7#v4@k&'
+SECRET_KEY = (
+    'django-insecure-x6q798#ez+ep(p)%d+1#z+88ptc-xz+=!f098=12j(a7#v4@k&'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
