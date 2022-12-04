d4ba8cb8486026336d84f7ae0689a5d07ab0b916
Grant Jenks <grant.jenks@gmail.com>
2022-12-03 16:17:54 -0800

    Remove old codegen files

--- www/codemirror-sql.js
+++
@@ -1,5 +0,0 @@
-import { StreamLanguage } from "@codemirror/language"
-import { standardSQL } from "@codemirror/legacy-modes/mode/sql"
-
-window.StreamLanguage = StreamLanguage
-window.standardSQL = standardSQL
--- www/y-codemirror.js
+++
@@ -1,7 +0,0 @@
-import * as Y from 'yjs'
-import { WebrtcProvider } from 'y-webrtc'
-import { yCollab } from 'y-codemirror.next'
-
-window.WebrtcProvider = WebrtcProvider
-window.Y = Y
-window.yCollab = yCollab
