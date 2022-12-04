1435ea83b1de78c1629488c4d7fc63d36e7efb24
Grant Jenks <grant.jenks@gmail.com>
2022-12-03 17:00:39 -0800

    Render demo at index

--- tests/www/urls.py
+++ tests/www/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django_codemirror6.views
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', django_codemirror6.views.demo),
     path('admin/', admin.site.urls),
 ]
