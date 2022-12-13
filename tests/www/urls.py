92ccbc3d5aad4eb6c5a9b1ce9acaa061f9c5c539
Grant Jenks <grant.jenks@gmail.com>
2022-12-12 22:36:49 -0800

    i sort it

--- tests/www/urls.py
+++ tests/www/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import django_codemirror6.views
-
 from django.contrib import admin
 from django.urls import path
+
+import django_codemirror6.views
 
 urlpatterns = [
     path('', django_codemirror6.views.demo),
