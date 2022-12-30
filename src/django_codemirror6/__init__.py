3ca07a0bfacaa0b9d274be658ba8a5e7a18a56f9
Grant Jenks <grant.jenks@gmail.com>
2022-12-30 15:41:51 -0800

    Bump version to 1.0.0 and add 2023 to copyright

--- src/django_codemirror6/__init__.py
+++ src/django_codemirror6/__init__.py
@@ -2,7 +2,7 @@
 """
 
 __title__ = 'django-codemirror6'
-__copyright__ = '2022, Grant Jenks'
+__copyright__ = '2022-2023, Grant Jenks'
 __author__ = 'Grant Jenks'
-__version__ = '0.0.2'
+__version__ = '1.0.0'
 __license__ = 'Apache 2.0'
